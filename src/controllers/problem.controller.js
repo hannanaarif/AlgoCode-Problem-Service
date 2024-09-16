1fd56cb34a2e94b63d1c7774436b950594cc2cbb
Aarif Hannan <hannanaarif@gmail.com>
2024-09-16 14:02:30 +0530

    Feat:Added error handler middleware with basic error classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,34 +1,41 @@
+const {StatusCodes}=require('http-status-codes')
+const BadRequest = require('../errors/badrequest.error');
+
 function pingpong(req,res){
    return res.json({"message":"ping from problem controll"});
 }
 
-function addProblem(req,res){
-     return res.status(501).json({
-        message:"Not Implemented"
-     })
+function addProblem(req,res,next){
+    try{
+        throw new BadRequest('Addproblem',{missing:['problem name']});
+    }
+    catch(error){
+        next(error);
+    }
 }
 
 function getProblem(){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"Not Implemented"
-     })
+     });
 }
 
 function getProblems(){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"Not Implemented"
-     })
+    });
 }
 
 function deleteProblem(){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message:"Not Implemented"
-     }) 
+     }); 
 }
 
 function updateProblem(){
-
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message:"Not Implemented"
+     });
 }
 
 module.exports={
